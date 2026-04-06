d10d66f9a5cc8d93c63ed42609d64157413d1fc1
vishalwork <134420544+vishalwork@users.noreply.github.com>
2026-04-06 13:02:51 +0530

    Update Sispack URL to version 1.2

--- Formula/sispack.rb
+++ Formula/sispack.rb
@@ -1,7 +1,7 @@
 class Sispack < Formula
   desc "Track and analyze installed package sizes on your system"
   homepage "https://github.com/vishalwork/sispack"
-  url "https://github.com/vishalwork/sispack/archive/refs/tags/v1.1.tar.gz"
+  url "https://github.com/vishalwork/sispack/archive/refs/tags/v1.2.tar.gz"
   sha256 "7bebae5b591bc6568a1c90e78d9210e7a8778ab68761d9a79f6a64f62e0c5ced"
   license "MIT"
 
