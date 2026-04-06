3f1171b512fa676533fdf955ca78ac15fd75b5f1
vishalwork <134420544+vishalwork@users.noreply.github.com>
2026-04-06 13:00:59 +0530

    Update sha256 checksum for sispack.rb

--- Formula/sispack.rb
+++ Formula/sispack.rb
@@ -2,7 +2,7 @@ class Sispack < Formula
   desc "Track and analyze installed package sizes on your system"
   homepage "https://github.com/vishalwork/sispack"
   url "https://github.com/vishalwork/sispack/archive/refs/tags/v1.1.tar.gz"
-  sha256 "4f428d2e1b768422761e86c1e2964cb827c8573895b045e31ad2446b28ed1cb3"
+  sha256 "7bebae5b591bc6568a1c90e78d9210e7a8778ab68761d9a79f6a64f62e0c5ced"
   license "MIT"
 
   def install
